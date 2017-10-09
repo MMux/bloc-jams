97a2aba92efe8061539960f22aa8e2172aa816c4
Michael Moss <michaeljmoss@gmail.com>
2017-10-09 08:17:16 -0700

    add window load function, scroll event for landing page selling points

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,13 +1,28 @@
-var points = document.getElementsByClassName('point');
+window.onload = function() {
+
+    // call animatePoints if window is > 950px tall
+    if (window.innerHeight > 950) {
+        animatePoints(pointsArray);
+    }
+
+    var sellingPoints = document.getElementsByClassName('selling-points')[0];
+
+    window.addEventListener('scroll', function(event) {
+        var scrollDistance = sellingPoints.getBoundingClientRect().top -window.innerHeight + 200;
+        if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
+            animatePoints(pointsArray);
+        }
+    });
+}
+
+var pointsArray = document.getElementsByClassName('point');
 
 var animatePoints = function(points) {
     var effectTransform = "scaleX(1) translateY(0)";
     for (var i = 0; i < points.length; i++) {
-        points[i].style.opacity = 1;
-        points[i].style.transform = effectTransform;
-        points[i].style.msTransform = effectTransform;
-        points[i].style.WebkitTransform = effectTransform;
+        pointsArray[i].style.opacity = 1;
+        pointsArray[i].style.transform = effectTransform;
+        pointsArray[i].style.msTransform = effectTransform;
+        pointsArray[i].style.WebkitTransform = effectTransform;
     }
 };
-
-animatePoints(points);
