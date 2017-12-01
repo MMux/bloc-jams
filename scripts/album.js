8bbf0627a70033f329c89e7fc8f8ec39cbb92041
Michael Moss <michaeljmoss@gmail.com>
2017-12-01 09:43:12 -0800

    remove equal sign

--- scripts/album.js
+++ scripts/album.js
@@ -79,28 +79,27 @@ var createSongRow = function(songNumber, songName, songLength) {
 
 window.onload = function() {
     setCurrentAlbum(albumPicasso);
-=======
-var songListContainer = document.getElementsByClassName('album-view-song-list')[0];
-var songRows = document.getElementsByClassName('album-view-song-item');
 
-// album button template
-var playButtonTemplate = '<a class="album-song-button"><span class="ion-play"></span></a>';
+    var songListContainer = document.getElementsByClassName('album-view-song-list')[0];
+    var songRows = document.getElementsByClassName('album-view-song-item');
 
-window.onload = function() {
-    setCurrentAlbum(albumPicasso);
+    // album button template
+    var playButtonTemplate = '<a class="album-song-button"><span class="ion-play"></span></a>';
 
-    songListContainer.addEventListener('mouseover', function(event) {
-        if (event.target.parentElement.className === 'album-view-song-item') {
-            // change song item number to play playButtonTemplate
-            event.target.parentElement.querySelector('.song-item-number').innerHTML = playButtonTemplate;
-        }
-    });
+    window.onload = function() {
+        setCurrentAlbum(albumPicasso);
 
-    for (let i = 0; i < songRows.length; i++) {
-        songRows[i].addEventListener('mouseleave', function(event) {
-            // revert to songNumber
-            this.children[0].innerHTML = this.children[0].getAttribute('data-song-number');
+        songListContainer.addEventListener('mouseover', function(event) {
+            if (event.target.parentElement.className === 'album-view-song-item') {
+                // change song item number to play playButtonTemplate
+                event.target.parentElement.querySelector('.song-item-number').innerHTML = playButtonTemplate;
+            }
         });
-    }
 
-}
+        for (let i = 0; i < songRows.length; i++) {
+            songRows[i].addEventListener('mouseleave', function(event) {
+                // revert to songNumber
+                this.children[0].innerHTML = this.children[0].getAttribute('data-song-number');
+            });
+        }
+    };
