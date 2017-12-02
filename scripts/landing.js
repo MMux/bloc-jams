03c8fa593d742144aa92f75f48b560d21bc7b09d
Michael Moss <michaeljmoss@gmail.com>
2017-12-01 18:00:15 -0800

    refactor landing page for jquery

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,30 +1,34 @@
-window.onload = function() {
+$(window).load (function() {
 
     // call animatePoints if window is > 950px tall
-    if (window.innerHeight > 950) {
-        animatePoints(pointsArray);
+    if ($(window).height() > 950) {
+        animatePoints();
     }
 
-    var sellingPoints = document.getElementsByClassName('selling-points')[0];
+    var scrollDistance = $('.selling-points').offset().top - $(window).height() + 200;
 
-    window.addEventListener('scroll', function(event) {
-        var scrollDistance = sellingPoints.getBoundingClientRect().top -window.innerHeight + 200;
-        if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
-            animatePoints(pointsArray);
+    $(window).scroll(function(event) {
+        if ($(window).scrollTop() >= scrollDistance) {
+            animatePoints();
         }
     });
-}
+});
 
-var pointsArray = document.getElementsByClassName('point');
 
-var animatePoints = function(target) {
-    forEach(target, transform);
+var animatePoints = function() {
+    var revealPoint = function() {
+        $(this).css({
+            opacity: 1,
+            transform: 'scaleX(1) translateY(0)'
+        });
+    };
+    $.each($('.point'), revealPoint);
 };
 
 var transform = function(element) {
-        var effectTransform = "scaleX(1) translateY(0)";
-        element.style.opacity = 1;
-        element.style.transform = effectTransform;
-        element.style.msTransform = effectTransform;
-        element.style.WebkitTransform = effectTransform;
+    var effectTransform = "scaleX(1) translateY(0)";
+    element.style.opacity = 1;
+    element.style.transform = effectTransform;
+    element.style.msTransform = effectTransform;
+    element.style.WebkitTransform = effectTransform;
 };
